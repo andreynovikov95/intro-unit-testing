93ab2c13e8d747655a5ec64e3634ccacac6cca68
Andrey Novikov <NovikovAA.it@relef.ru>
2021-07-25 01:45:41 +0500

    feat: add axios test

--- 03_async/async.spec.js
+++ 03_async/async.spec.js
@@ -1,28 +1,59 @@
+const { default: axios } = require('axios')
 const Ajax = require('./async')
 
+jest.mock('axios')
+
 describe('Ajax: echo', () => {
-    test('shoud return value async', async () => {
+    test('should return value async', async () => {
         const result = await Ajax.echo('some data')
         expect(result).toBe('some data')
     })
 
-    test('shoud return value with promise', () => {
+    test('should return value with promise', () => {
         return Ajax.echo('some data').then(data => {
             expect(data).toBe('some data')
         })
     })
 
-    test('shoud catch error with promise', () => {
+    test('should catch error with promise', () => {
         return Ajax.echo('some data').catch(err => {
             expect(err).toBeInstanceOf(Error)
         })
     })
 
-    test('shoud catch error with promise', async () => {
+    test('should catch error with promise', async () => {
         try {
             await Ajax.echo()
         } catch (e) {
             expect(e.message).toBe('error')
         }
+    })
+})
+
+describe('Ajax: GET', () => {
+    let response
+    let todos
+
+    beforeEach(() => {
+        todos = [
+            {
+                id: 1,
+                title: 'Todo 1',
+                completed: false
+            }
+        ]
+
+        response = {
+            data: {
+                todos
+            }
+        }
+    })
+    test('should return data from backend', () => {
+        axios.get.mockReturnValue(response)
+
+        return Ajax.get().then(data => {
+            expect(data.todos).toEqual(todos)
+        })
     })
 })
