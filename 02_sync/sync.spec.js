859fb8eddd94c2524078acb4c1ecc37b59954c54
Andrey Novikov <NovikovAA.it@relef.ru>
2021-07-25 00:53:58 +0500

    feat: add sync tests

---
+++ 02_sync/sync.spec.js
@@ -0,0 +1,74 @@
+const Lodash = require('./sync');
+
+let _ = new Lodash()
+
+describe('Lodash: compact', () => {
+    let array
+
+    beforeEach(() => {
+        array = [false, 42, 0, '', true, null, 'hello']
+    })
+
+    afterAll(() => {
+        _ = new Lodash()
+    })
+
+    test('should be defined', () => {
+        expect(_.compact).toBeDefined()
+        expect(_.compact).not.toBeUndefined()
+    })
+
+    test('should working array be editable', () => {
+        array.push(...['one', 'two'])
+        expect(array).toContain('one')
+        expect(array).toContain('two')
+    })
+
+    test('should NOT contain falsy values', () => {
+        expect(_.compact(array)).not.toContain(false)
+        expect(_.compact(array)).not.toContain(0)
+        expect(_.compact(array)).not.toContain('')
+        expect(_.compact(array)).not.toContain(null)
+    })
+
+    test('should remove falsy values from array', () => {
+        const result = [42, true, 'hello']
+
+        expect(_.compact(array)).toEqual(result)
+    })
+});
+
+describe('Lodash: groupBy', () => {
+    let array
+
+    beforeEach(() => {
+        array = [2.2, 2.4, 4.2, 3.1]
+    })
+
+    test('should be defined', () => {
+        expect(_.groupBy).toBeDefined()
+        expect(_.groupBy).not.toBeUndefined()
+    })
+
+    test('should NOT return ', () => {
+        expect(_.groupBy([], Math.trunc)).not.toBeInstanceOf(Array)
+    })
+
+    test('should group aaray items by Math.floor', () => {
+        const result = {
+            2: [2.2, 2.4],
+            4: [4.2],
+            3: [3.1]
+        }
+        expect(_.groupBy(array, Math.floor)).toEqual(result)
+    })
+
+    test('should group aaray items by length', () => {
+        const array = ['one', 'two', 'three']
+        const result = {
+            5: ['three'],
+            3: ['one', 'two']
+        }
+        expect(_.groupBy(array, 'length')).toEqual(result)
+    })
+});
